4c6d2f3dc0fed545f1b65305a6fdae5ab07e9759
Lavoie, Carl-Eric <carl-eric.lavoie@sap.com>
2016-10-29 11:47:16 -0400

    Using default value for save param for easier readability. Also adding a clean method so that multiple calls to load act properly.

--- lib/schedulr/schedulr.rb
+++ lib/schedulr/schedulr.rb
@@ -4,21 +4,9 @@ require 'schedulr/activity'
 
 module Schedulr
 
-  # proc used to get current time. Will be changed when reconstructing app state
-  @time_now = Time.now
-  #calendar object
-  @calendar = Calendar.new
-  #name of the instance to load
-  @instance_name = nil
-  #list of all activities
-  @activities = Array.new
-  #current activity
-  @current_activity = nil
-  #timestamp of the latest event. used to calculate time diff between events.
-  @latest_timestamp = nil
-
 
   def self.load(name)
+    initialize_state()
     @instance_name = name
     open(name, 'a')
     open(name, 'r+') do |f|
@@ -29,13 +17,28 @@ module Schedulr
     nil
   end
 
+  def self.initialize_state()
+    # proc used to get current time. Will be changed when reconstructing app state
+    @time_now = Time.now
+    #calendar object
+    @calendar = Calendar.new
+    #name of the instance to load
+    @instance_name = nil
+    #list of all activities
+    @activities = Array.new
+    #current activity
+    @current_activity = nil
+    #timestamp of the latest event. used to calculate time diff between events.
+    @latest_timestamp = nil
+  end
+
   def self.load_line(line)
     log = LogEntry.from(line)
     @time_now = log.date
     send(log.cmd, *log.args, false)
   end
 
-  def self.rename(activity_id, activity_name, save)
+  def self.rename(activity_id, activity_name, save = true)
     activity_id = activity_id.to_i
 
     save ("rename", [activity_id, activity_name]) if save
@@ -52,7 +55,7 @@ module Schedulr
     end
   end
 
-  def self.add(activity, save)
+  def self.add(activity, save = true)
     save ("add", [activity]) if save
     activity = Activity.new(activity)
     @activities << activity
@@ -89,13 +92,13 @@ module Schedulr
   #Adds an event to the right day
 
 
-  def self.start(save)
+  def self.start(save = true)
     save("start", []) if save
     @timer_running = true if !@current_activity.nil?
     @latest_timestamp = @time_now
   end
 
-  def self.stop(save)
+  def self.stop(save = true)
     @temp =true
     save("stop", []) if save
     computeTime()
@@ -106,13 +109,13 @@ module Schedulr
     @current_activity
   end
 
-  def self.set (activity_id, save)
+  def self.set (activity_id, save = true)
     save("set", [activity_id]) if save
     computeTime()
     @current_activity = get(activity_id)
   end
 
-  def self.remove(activity_id, save)
+  def self.remove(activity_id, save = true)
     activity_id = activity_id.to_i
 
     save ("remove", [activity_id]) if save
